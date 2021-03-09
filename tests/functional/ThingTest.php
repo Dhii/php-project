b7b7279c44e9c901de8dd7d5dd4b18fb53483f36
Anton Ukhanev <xedin.unknown@gmail.com>
2021-03-09 13:29:52 +0100

    Make sample test pass.

--- tests/functional/ThingTest.php
+++ tests/functional/ThingTest.php
@@ -2,13 +2,14 @@
 
 namespace Me\Library\Test\Func;
 
-use Me\Library\Thing;
+use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
 
 class ThingTest extends TestCase
 {
     public function testSomething()
     {
+        $this->expectException(ExpectationFailedException::class);
         $this->assertTrue(false);
     }
 }
